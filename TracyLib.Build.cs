0076c7269436e287131ee1e96bbc9538d020f627
Zeblote <zeblote@gmail.com>
2024-05-05 04:50:10 +0200

    [BR] Register the module.

---
+++ TracyLib.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Brickadia, LLC. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class TracyLib : ModuleRules
+{
+	public TracyLib(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "public"));
+	}
+}
